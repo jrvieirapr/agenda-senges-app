941a548a255d74c5a5c78c950fdcc4a652c8e013
jrvieirapr <105877022+jrvieirapr@users.noreply.github.com>
2024-09-18 14:52:51 +0000

    Botao Editar

--- agenda-app/resources/views/eventos/index.blade.php
+++ agenda-app/resources/views/eventos/index.blade.php
@@ -4,9 +4,9 @@
 @section('content')
 <h1>Agenda</h1>
 <!-- href="{{route('eventos.create')}}" -->
- <!-- "/eventos/create"  -->
-<a href="/eventos/create" 
-class="btn btn-primary">Novo</a>
+<!-- "/eventos/create"  -->
+<a href="/eventos/create"
+    class="btn btn-primary">Novo</a>
 <div>
     <table class='table mt-3 table-bordered text-center'>
         <thead>
@@ -22,27 +22,31 @@ class="btn btn-primary">Novo</a>
         <tbody>
             @foreach($eventos as $evento)
             <!-- evento.id -->
-             <tr>
-                 <td>{{ $evento->id}}</td>
-                 <td>{{ $evento->data}}</td>
-                 <td>{{ $evento->descricao}}</td>
-                 <td>{{ $evento->inicio}}</td>
-                 <td>{{ $evento->final}}</td>
-                 <td>{{ $evento->contato}}</td>
-                 <td>{{ $evento->realizado}}</td>
-                 <td>editar</td>
-                 <td>
+            <tr>
+                <td>{{ $evento->id}}</td>
+                <td>{{ $evento->data}}</td>
+                <td>{{ $evento->descricao}}</td>
+                <td>{{ $evento->inicio}}</td>
+                <td>{{ $evento->final}}</td>
+                <td>{{ $evento->contato}}</td>
+                <td>{{ $evento->realizado}}</td>
+                <td>
+                    <a href="/eventos/{{$evento->id}}"
+                        class="btn btn-warning">
+                        <i class="bi bi-pencil"></i>                        
+                    </a>
+                </td>
+                <td>
                     <!-- route('eventos.destroy', $evento) -->
                     <form action="/eventos/{{$evento->id}}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button class="btn btn-danger">
-                        <i class="bi bi-trash"> </i>
-                        Remover
+                        <button type="submit" class="btn btn-danger">
+                            <i class="bi bi-trash"> </i>                            
                         </button>
                     </form>
-                 </td>
-             </tr>
+                </td>
+            </tr>
             @endforeach
         </tbody>
     </table>
